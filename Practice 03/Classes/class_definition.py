117862e2f9146d308114ce3d4a57bc4701a55d96
Ho1yMoly4 <r_batyrbekov@kbtu.kz>
2026-02-10 14:46:45 +0500

    .

---
+++ Practice 03/Classes/class_definition.py
@@ -0,0 +1,22 @@
+class MyClass:
+  x = 5
+
+________________________ 
+
+p1 = MyClass()
+print(p1.x)
+
+________________________ 
+
+p1 = MyClass()
+p2 = MyClass()
+p3 = MyClass()
+
+print(p1.x)
+print(p2.x)
+print(p3.x)
+
+________________________ 
+
+class Person:
+  pass
