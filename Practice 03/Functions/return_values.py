86f424efa24bff1ffcfac3a334aed034c7561880
Ho1yMoly4 <r_batyrbekov@kbtu.kz>
2026-02-10 14:33:26 +0500

    .

---
+++ Practice 03/Functions/return_values.py
@@ -0,0 +1,43 @@
+def get_greeting():
+  return "Hello from a function"
+
+message = get_greeting()
+print(message)
+
+
+________________________
+
+def get_greeting():
+  return "Hello from a function"
+
+print(get_greeting())
+
+
+________________________ 
+
+def my_function(x, y):
+  return x + y
+
+result = my_function(5, 3)
+print(result)
+
+
+________________________ 
+
+def my_function():
+  return ["apple", "banana", "cherry"]
+
+fruits = my_function()
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+
+
+________________________ 
+
+def my_function():
+  return (10, 20)
+
+x, y = my_function()
+print("x:", x)
+print("y:", y)
